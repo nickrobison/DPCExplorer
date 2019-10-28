25ff22d87417e188c858eb63c915547d52f40e65
Nick Robison <nick@nickrobison.com>
2019-10-27 21:41:49 -0400

    Initial Commit

---
+++ DPCExplorer/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  DPCExplorer
+//
+//  Created by Nicholas Robison on 10/27/19.
+//  Copyright © 2019 Nicholas Robison. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
