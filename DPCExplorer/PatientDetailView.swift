95edf2f12d4033ffe16bde0734e400142b3a92b4
Nick Robison <nick@nickrobison.com>
2019-11-25 22:50:34 -0500

    Added tabs on Patient Detail View
    Lots of really, really gross AnyView madness, but it does seem to be working ok.

--- DPCExplorer/PatientDetailView.swift
+++ DPCExplorer/PatientDetailView.swift
@@ -8,29 +8,79 @@
 
 import SwiftUI
 
+struct PatientTabView: View {
+    
+    let viewIdx: Int
+    let patient: PatientEntity
+    
+    var body: some View {
+        switch viewIdx {
+        case 0:
+            return AnyView(buildOverview())
+        case 1:
+            return AnyView(buildProviders())
+        case 2:
+            return buildClaims()
+        default:
+            return AnyView(buildDefault())
+        }
+    }
+    
+    private func buildOverview() -> some View {
+        return Text("Overview")
+    }
+    
+    private func buildProviders() -> some View {
+        return List(self.patient.providerRelationship?.allObjects as! [ProviderEntity], id: \.self) { provider in
+            PersonCellView(person: provider)
+        }
+    }
+    
+    private func buildClaims() -> AnyView  {
+        if (self.patient.eob != nil) {
+            return AnyView(buildWithClaims())
+        } else {
+            return AnyView(buildNoClaims())
+        }
+    }
+    
+    private func buildWithClaims() -> some View {
+        return HStack {
+            Text("Fetched EOBs")
+            Image(systemName: "checkmark.circle.fill")
+                .font(.subheadline)
+                .foregroundColor(.green)
+            
+        }
+    }
+    
+    private func buildNoClaims() -> some View {
+        return EmptyView()
+    }
+    
+    private func buildDefault() -> some View {
+        return EmptyView()
+    }
+}
+
 struct PatientDetailView: View {
+    @State private var selectorIndex = 0
     let patient: PatientEntity
     var body: some View {
         VStack {
             PatientBioView(patient: patient)
-            .padding()
+                .padding()
             Divider()
-            List(patient.providerRelationship?.allObjects as! [ProviderEntity], id: \.self) { provider in
-                PersonCellView(person: provider)
+            Picker("Tabs", selection: $selectorIndex) {
+                Text("Overview").tag(0)
+                Text("Providers").tag(1)
+                Text("Claims").tag(2)
+                
             }
-            Divider()
-            if (patient.eob != nil) {
-                HStack {
-                    Text("Fetched EOBs")
-                    Image(systemName: "checkmark.circle.fill")
-                        .font(.subheadline)
-                        .foregroundColor(.green)
-                }
-            } else {
-                EmptyView()
-            }
-            Spacer()
+            .pickerStyle(SegmentedPickerStyle())
+            PatientTabView(viewIdx: $selectorIndex.wrappedValue, patient: self.patient)
             
+            Spacer()
         }
     }
 }
